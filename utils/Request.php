8405a996b56ab8d108fd003a4778f966fd71bf7d
Ebuka Odini <ebukaodini@gmail.com>
2021-09-09 19:30:49 +0100

    added the request and response class

--- utils/Request.php
+++ utils/Request.php
@@ -4,7 +4,12 @@ namespace Utils;
 
 class Request
 {
-  public static function configureRequest() {
-    header('accept: applcation/json');
+  public static function configureRequest()
+  {
+    header('Accept: */*');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Content-Type, X-Custom-Header, AuthToken");
+    // AuthToken is the jwt token sent after the user login
   }
 }
--- utils/Response.php
+++ utils/Response.php
@@ -6,18 +6,23 @@ class Response
 {
   public static function success(string $message = 'success', array $data = [], int $code = 200,)
   {
+    header('Content-type: application/json');
     http_response_code($code);
     exit(json_encode([
       'code' => $code,
+      'status' => 'success',
       'message' => $message,
       'data' => $data
     ]));
   }
+  
   public static function error(string $message = 'error', array $data = [], int $code = 400)
   {
+    header('Content-type: application/json');
     http_response_code($code);
     exit(json_encode([
       'code' => $code,
+      'status' => 'error',
       'message' => $message,
       'data' => $data
     ]));
